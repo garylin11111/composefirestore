0fe0b4e84c3bb866e248ce0a9f18c2bd34a0bbce
garylin11111 <garry240993@gmail.com>
2024-11-04 15:57:06 +0800

    v4 按鈕

--- app/src/main/java/tw/composefirestore/MainActivity.kt
+++ app/src/main/java/tw/composefirestore/MainActivity.kt
@@ -6,9 +6,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -52,6 +54,7 @@ fun Birth(m: Modifier){
     var userName by remember { mutableStateOf("林政彥")}
     var userWeight by remember { mutableStateOf(4000)}
     var userPassword by remember { mutableStateOf("")}
+    var msg by remember { mutableStateOf("訊息")}
 
     Column {
         TextField(
@@ -89,5 +92,17 @@ fun Birth(m: Modifier){
         )
         Text("您輸入的姓名是：$userName\n出生體重為：$userWeight 公克"
                 + "\n密碼：$userPassword")
+        Row {
+            Button(onClick = {  }) {
+                Text("新增/修改資料")
+            }
+            Button(onClick = {  }) {
+                Text("查詢資料")
+            }
+            Button(onClick = {  }) {
+                Text("刪除資料")
+            }
+        }
+        Text(text = msg)
     }
-    }
+}
